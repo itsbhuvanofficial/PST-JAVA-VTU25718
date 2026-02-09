9d11c02c430ddad22a899f408a8bc0b1645248ed
itsbhuvanofficial <bhuvanesh.bellamkonda@gmail.com>
2026-02-09 14:20:00 +0530

    Add files via upload

---
+++ PST-JAVA/WEEK 2/TASK 10.JAVA
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+class UserMainCode
+{
+    public static int sumOfSumsOfDigits(int input1)
+    {
+        int totalSum = 0;
+        String num = String.valueOf(input1);
+        int currentSum = 0;
+        for (int i = 0; i < num.length(); i++)
+        {
+            currentSum = currentSum + (num.charAt(i) - '0');
+        }
+        totalSum = totalSum + currentSum;
+        for (int i = 0; i < num.length() - 1; i++)
+        {
+            int digit = num.charAt(i) - '0';
+            currentSum = currentSum - digit;
+            totalSum = totalSum + currentSum;
+        }
+
+        return totalSum;
+    }
+}
